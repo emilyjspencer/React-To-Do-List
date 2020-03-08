cac30892ce76d30f1c6d7a1915e1753e6ed1a8b2
lajoiedevivre89 <emilyspencer89@hotmail.co.uk>
2020-03-08 21:05:10 +0000

    Updates app.js - main component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
       <h1>App</h1>
       
     
     </div>
   );
+}
 }
 
 export default App;
