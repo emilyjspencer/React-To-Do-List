3739e150c93c2d107465193da47a4f75cab890ba
lajoiedevivre89 <emilyspencer89@hotmail.co.uk>
2020-03-08 21:13:38 +0000

    Imports Todos component into app.js and embeds Todos component in div

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from 'react';
+import Todos from './components/Todos';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      <h1>App</h1>
+    <Todos />
       
     
     </div>
