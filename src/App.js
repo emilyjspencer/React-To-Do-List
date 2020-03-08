d02331682f9b4d4b8e9435272050b4323a87b1b2
lajoiedevivre89 <emilyspencer89@hotmail.co.uk>
2020-03-08 21:16:27 +0000

    Adds state - list of todos

--- src/App.js
+++ src/App.js
@@ -2,6 +2,35 @@ import React, { Component } from 'react';
 import Todos from './components/Todos';
 
 class App extends Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: 'Meditate',
+        completed: false
+      },
+      {
+        id: 2,
+        title: 'Go for a run',
+        completed: false
+      },
+      {
+        id: 3,
+        title: 'Prepare lunches',
+        completed: false
+      },
+      {
+        id: 4,
+        title: 'Ring people',
+        completed: false
+      },
+      {
+        id: 5,
+        title: 'Paint nails',
+        completed: false
+      }
+    ]
+  }
   render() {
     return (
       <div className="App">
